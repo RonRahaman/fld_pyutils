8d46d7d20971c7ee80bca81c07f54e6187035925
RonRahaman <ron.rahaman@outlook.com>
2019-01-16 09:43:25 -0600

    Reading first row after header (global element numbers)

--- fld_reader.py
+++ fld_reader.py
@@ -1,31 +1,53 @@
 import struct
 import numpy as np
+from collections import namedtuple
+
+HeaderInfo = namedtuple('HeaderInfo', [
+    'float_size',
+    'nx',
+    'ny',
+    'nz',
+    'nelt',
+    'nelgt',
+    'time',
+    'iostep',
+    'fid0',
+    'nfileoo',
+    'rdcode',
+    'p0th',
+    'if_press_mesh',
+    'float_fmt',
+    'int_size',
+    'int_fmt'])
 
 with open('data/test0.f00001', 'rb') as f:
     header = f.read(132).decode(encoding='ascii')
     header_list = header.split()
 
-    hdict = dict(
-        wdsize=int(header_list[1]),
+    hinfo = HeaderInfo(
+        float_size=int(header_list[1]),
         nx=int(header_list[2]),
         ny=int(header_list[3]),
         nz=int(header_list[4]),
-        nelo=int(header_list[5]),
+        nelt=int(header_list[5]),
         nelgt=int(header_list[6]),
         time=float(header_list[7]),
         iostep=int(header_list[8]),
         fid0=int(header_list[9]),
         nfileoo=int(header_list[10]),
-        rdcode=header_list[11])
+        rdcode=header_list[11],
+        p0th=float(header_list[12]),
+        if_press_mesh=False if 'F' in header_list[13] else True,
+        float_fmt='f' if int(header_list[1]) == 4 else 'd',
+        int_size=4,
+        int_fmt='i'
+    )
 
-    print(hdict)
+    print(header_list)
+    print(hinfo)
 
-    float_fmt = 'f' if hdict['wdsize'] == 4 else 'd'
+    endian = np.fromfile(f, dtype=np.float32, count=1)[0]
 
-    endian = struct.unpack('f', f.read(4))
-
-    nxyz = hdict['nx'] * hdict['ny'] * hdict['nz']
-
-    # TODO: garbage
-    vals = np.array(struct.unpack(float_fmt * nxyz, f.read(nxyz * hdict['wdsize'])), dtype=float_fmt)
-    print(vals)
+    # TODO: Make dtype a variable
+    glob_el_nums = np.fromfile(f, dtype=np.int32, count=hinfo.nelt)
+    print(glob_el_nums)
