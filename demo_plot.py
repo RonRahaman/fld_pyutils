f5fb3636aa9083bd9b0206e48b059433e173e14a
RonRahaman <rahaman@cs.uchicago.edu>
2020-09-18 15:42:47 -0500

    Moved plotting to base class

--- demo_plot.py
+++ demo_plot.py
@@ -1,5 +1,6 @@
 from fld_data_memmap import FldDataMemmap
+from fld_data import FldData
 
 #d1 = FldDataMemmap.fromfile('demos/data/test0.f00001')
-d1 = FldDataMemmap.fromfile('testA/rod_short0.f00001')
+d1 = FldData.fromfile('testA/rod_short0.f00001')
 d1.plot()
--- fld_data_base.py
+++ fld_data_base.py
@@ -2,6 +2,7 @@ import numpy as np
 from fld_header import FldHeader
 from abc import ABC, abstractmethod
 import typing
+import vtk
 
 
 class FldDataBase(ABC):
@@ -237,3 +238,235 @@ class FldDataBase(ABC):
         if self.s.size != 0:
             rdcode += "S{:02}".format(self.s.shape[0])
         self._header.rdcode = rdcode
+
+    def make_hex(self, e):
+        # For debugging
+        nx = self.nx1   # Assume nx1 == ny1 == nz1
+        gpt = lambda x, y, z: (e * nx**3) + (z * nx**2) + (y * nx) + x  # Get the gridpoint corresponding to an (x, y, z) coordinate
+
+        # Start with vertices
+        idx = [
+            gpt(0, 0, 0),  # Vertex 0
+            gpt(nx-1, 0, 0),  # Vertex 1
+            gpt(nx-1, nx-1, 0),  # Vertex 2
+            gpt(0, nx-1, 0), # Vertex 3
+            gpt(0, 0, nx-1), # Vertex 4
+            gpt(nx-1, 0, nx-1), # Vertex 5
+            gpt(nx-1, nx-1, nx-1), # Vertex 6
+            gpt(0, nx-1, nx-1), # Vertex 8
+        ]
+
+        # Edge 8
+        idx.append(gpt(nx//2, 0, 0))
+        # Edge 9
+        idx.append(gpt(nx-1, nx//2, 0))
+        # Edge 10
+        idx.append(gpt(nx//2, nx-1, 0))
+        # Edge 11
+        idx.append(gpt(0, nx//2, 0))
+        # Edge 12
+        idx.append(gpt(nx//2, 0, nx-1))
+        # Edge 13
+        idx.append(gpt(nx-1, nx//2, nx-1))
+        # Edge 14
+        idx.append(gpt(nx//2, nx-1, nx-1))
+        # Edge 15
+        idx.append(gpt(0, nx//2, nx-1))
+        # Edge 16
+        idx.append(gpt(0, 0, nx//2))
+        # Edge 17
+        idx.append(gpt(nx-1, 0, nx//2))
+        # Edge 18
+        idx.append(gpt(nx-1, nx-1, nx//2))
+        # Edge 19
+        idx.append(gpt(0, nx-1, nx//2))
+
+        hex = vtk.vtkQuadraticHexahedron()
+        hex.GetPointIds().SetNumberOfIds(25)
+        hex.GetPoints().SetNumberOfPoints(25)
+        hex.Initialize()
+        for i, x in enumerate(idx):
+            hex.GetPointIds().SetId(i, x)
+        return hex
+
+    def make_lagrange_hex(self, e):
+        nx = self.nx1   # Assume nx1 == ny1 == nz1
+        gpt = lambda r, s, t: (e * nx**3) + r * nx**2 + s * nx + t  # Get the gridpoint corresponding to an (x, y, z) coordinate
+
+        # From https://blog.kitware.com/wp-content/uploads/2018/09/Source_Issue_43.pdf
+        # with some corrections due to inconsistencies in diagram (see notes below)
+
+        # Start with vertices
+        idx = [
+            gpt(0, 0, 0),  # Vertex 0
+            gpt(nx-1, 0, 0),  # Vertex 1
+            gpt(nx-1, nx-1, 0),  # Vertex 2
+            gpt(0, nx-1, 0), # Vertex 3
+            gpt(0, 0, nx-1), # Vertex 4
+            gpt(nx-1, 0, nx-1), # Vertex 5
+            gpt(nx-1, nx-1, nx-1), # Vertex 6
+            gpt(0, nx-1, nx-1), # Vertex 8
+        ]
+
+        # Edge 8,9
+        for i in range(1, nx-1):
+            idx.append(gpt(i, 0, 0))
+        # Edge 10,11
+        for i in range(1, nx-1):
+            idx.append(gpt(nx-1, i, 0))
+        # Edge 12,13
+        for i in range(1, nx-1):
+            idx.append(gpt(i, nx-1, 0))
+        # Edge 14, 15
+        for i in range(1, nx-1):
+            idx.append(gpt(0, i, 0))
+        # Edge 16, 17
+        for i in range(1, nx-1):
+            idx.append(gpt(i, 0, nx-1))
+        # Edge 18, 19
+        for i in range(1, nx-1):
+            idx.append(gpt(nx-1, i, nx-1))
+        # Edge 20, 21
+        for i in range(1, nx-1):
+            idx.append(gpt(i, nx-1, nx-1))
+        # Edge 22, 23
+        for i in range(1, nx-1):
+            idx.append(gpt(0, i, nx-1))
+        # Edge 24, 26
+        for i in range(1, nx-1):
+            idx.append(gpt(0, 0, i))
+        # Edge 26, 27
+        for i in range(1, nx-1):
+            idx.append(gpt(nx-1, 0, i))
+
+        # /****** Swapped these two edges vs blog ******
+        # Edge 30, 31
+        for i in range(1, nx-1):
+            idx.append(gpt(0, nx-1, i))
+        # Edge 28, 29
+        for i in range(1, nx-1):
+            idx.append(gpt(nx-1, nx-1, i))
+        # **********************************************/
+
+        # /****** Swapped these faces vs blog ***********
+        # Face 40
+        for t in range(1, nx-1):
+            for s in range(1, nx-1):
+                idx.append(gpt(0, s, t))
+        # Face 44
+        for t in range(1, nx-1):
+            for s in range(1, nx-1):
+                idx.append(gpt(nx-1, s, t))
+        # Face 32
+        for t in range(1, nx-1):
+            for r in range(1, nx - 1):
+                idx.append(gpt(r, 0, t))
+        # Face 36
+        for t in range(1, nx-1):
+            for r in range(1, nx - 1):
+                idx.append(gpt(r, nx-1, t))
+        # **********************************************/
+
+        # Face 48
+        for s in range(1, nx-1):
+            for r in range(1, nx-1):
+                idx.append(gpt(r, s, 0))
+        # Face 52
+        for s in range(1, nx-1):
+            for r in range(1, nx-1):
+                idx.append(gpt(r, s, nx-1))
+
+        # Interior
+        for t in range(1, nx - 1):
+            for s in range(1, nx-1):
+                for r in range(1, nx - 1):
+                    idx.append(gpt(r, s, t))
+
+        hex = vtk.vtkLagrangeHexahedron()
+        hex.GetPointIds().SetNumberOfIds(nx**3)
+        hex.GetPoints().SetNumberOfPoints(nx**3)
+        hex.Initialize()
+        for i, x in enumerate(idx):
+            hex.GetPointIds().SetId(i, x)
+        return hex
+
+    def plot(self):
+        colors = vtk.vtkNamedColors()
+
+        N = self.nelt
+        n_gll = self.nx1 * self.ny1 * self.nz1
+
+        # Setup the points
+
+        points = vtk.vtkPoints()
+        points.Allocate(N * n_gll)
+
+        scalars = vtk.vtkDoubleArray()
+        scalars.Allocate(N * n_gll)
+
+        for e in range(N):
+            for i in range(n_gll):
+                pt = [self.coords[e,0,i], self.coords[e,1,i], self.coords[e,2,i]]
+                points.InsertNextPoint(pt)
+                scalars.InsertNextTuple1(self.t[e,i])
+
+        # Setup the grid and cells
+
+        hex_grid = vtk.vtkUnstructuredGrid()
+        hex_grid.Allocate(N)
+
+        for i in range(N):
+            hex = self.make_lagrange_hex(i)
+            hex_grid.InsertNextCell(hex.GetCellType(), hex.GetPointIds())
+            print("\rProcessed {} / {} elements ...".format(i, N), end='')
+        print(" done!")
+        hex_grid.SetPoints(points)
+        hex_grid.GetPointData().SetScalars(scalars)
+
+        # Clipping:  https://lorensen.github.io/VTKExamples/site/Python/UnstructuredGrid/ClipUnstructuredGridWithPlane2/
+
+        clip_plane = vtk.vtkPlane()
+        clip_plane.SetOrigin(hex_grid.GetCenter())
+        clip_plane.SetNormal([1.0, 0.0, 0.0])
+
+        clipper = vtk.vtkClipDataSet()
+        clipper.SetClipFunction(clip_plane)
+        clipper.SetInputData(hex_grid)
+        clipper.SetValue(0.0)
+        clipper.GenerateClippedOutputOn()
+        clipper.Update()
+
+        mapper = vtk.vtkDataSetMapper()
+        mapper.SetInputData(clipper.GetOutput())
+        mapper.SetScalarRange(hex_grid.GetScalarRange())
+
+        #mapper = vtk.vtkDataSetMapper()
+        #mapper.SetInputData(hex_grid)
+        #mapper.SetScalarRange(hex_grid.GetScalarRange())
+
+        # Plot it!
+
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+        # actor.GetProperty().SetRepresentationToWireframe()
+        #actor.GetProperty().SetColor(colors.GetColor3d("Peacock"))
+        actor.GetProperty().EdgeVisibilityOn()
+        actor.GetProperty().SetLineWidth(0.25)
+        actor.GetProperty().SetAmbient(50)
+
+
+        ren = vtk.vtkRenderer()
+        ren.AddActor(actor)
+        ren.SetBackground(colors.GetColor3d("Beige"))
+
+        ren_win = vtk.vtkRenderWindow()
+        ren_win.AddRenderer(ren)
+        ren_win.SetSize(1600, 800)
+
+        iren = vtk.vtkRenderWindowInteractor()
+        iren.SetRenderWindow(ren_win)
+
+        ren_win.Render()
+
+        iren.Initialize()
+        iren.Start()
