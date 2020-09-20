124f3578eef059f17faebadd1dc32374a73b2c50
RonRahaman <rahaman@cs.uchicago.edu>
2020-09-20 13:34:04 -0500

    Cleaned up positions in C++ fld header

--- cxx/include/FldHeader.h
+++ cxx/include/FldHeader.h
@@ -17,6 +17,7 @@ public:
 
   explicit FldHeader(const std::string &filename);
 
+  std::string Filename;
   std::size_t Nx1;
   std::size_t Ny1;
   std::size_t Nz1;
@@ -28,16 +29,20 @@ public:
   int Fid0;
   int Nfileoo;
   double P0th;
-  bool IfPressMesh;
+  bool IfPressMesh = false;
+
+  const std::streampos EndianTestBegin = 132;  // Position where binary portion of file begins
+  const std::streampos GlelBegin = 136;        // Position where glel begins
+  std::streampos FieldsBegin;
+
+  const float EndianExpectVal = 6.54321f;  // Expected value for endianness test
 
   std::vector<IntType> Glel;
 };
 
 template <typename floatTypeT, typename intTypeT>
-FldHeader<floatTypeT, intTypeT>::FldHeader(const std::string& filename)
+FldHeader<floatTypeT, intTypeT>::FldHeader(const std::string& filename) : Filename(filename)
 {
-  std::streampos binaryPos = 132;  // Position where binary portion of file begins
-  float endianExpectVal = 6.54321f;  // Expected value for endianness test
 
   std::size_t floatSizeTest; // Used to test size of float type in file
   std::string pressMeshKey; // Used to determine if_press_mesh
@@ -45,7 +50,7 @@ FldHeader<floatTypeT, intTypeT>::FldHeader(const std::string& filename)
   // The header is ASCII-encoded
   try
   {
-    std::ifstream infile(filename);
+    std::ifstream infile(Filename);
     infile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
     std::string scratch;      // Throwaway first token
     infile >> scratch >> floatSizeTest >> Nx1 >> Ny1 >> Nz1 >> Nelt >> Nelgt >> Time >> Iostep >>
@@ -53,42 +58,54 @@ FldHeader<floatTypeT, intTypeT>::FldHeader(const std::string& filename)
   }
   catch (std::ifstream::failure& e)
   {
-    std::cerr << e.what() << ": Couldn't open or read fld.file in ASCII mode: " << filename
+    std::cerr << e.what() << ": Couldn't open or read fld.file in ASCII mode: " << Filename
               << std::endl;
     throw e;
   }
 
+  // See if fld file has double or single precision data.  Usually float
+  // TODO: Handle different-sized floats.  Probably factory
   if (floatSizeTest != sizeof(FloatType))
   {
-    // TODO: Handle different-sized floats.  Probably factory
-    throw std::runtime_error("Error: .fld file " + filename + " has " + std::to_string(floatSizeTest) +
+    throw std::runtime_error("Error: .fld file " + Filename + " has " + std::to_string(floatSizeTest) +
       "-byte floats, but the FldReader was instantiated to read " + std::to_string(sizeof(FloatType)) + "byte floats");
   }
+
+  // See if there's a pressure mesh
+  if (pressMeshKey == "t" || pressMeshKey == "T") {
+    throw std::runtime_error("Error: .fld file " + Filename
+      + "specifies if_press_mesh=F, but PnPn-2 is not supported.");
+  }
+
 
   // The rest of the data is binary
   try
   {
-    std::ifstream infile(filename, std::ifstream::binary);
-    infile.seekg(binaryPos, std::ifstream::beg);
+    std::ifstream infile(Filename, std::ifstream::binary);
 
     // Test endianness.  TODO: Support non-natives endiannesses
     float endianTestVal;
+    infile.seekg(EndianTestBegin, std::ifstream::beg);
     infile.read(reinterpret_cast<char*>(&endianTestVal), sizeof(float));
-    if (endianTestVal != endianExpectVal) {
-      throw std::runtime_error("Endianness of .fld file " + filename
+    if (endianTestVal != EndianExpectVal) {
+      throw std::runtime_error("Endianness of .fld file " + Filename
                                  + " does not match endianness of current system.");
     }
 
     // Read Glel
     Glel.resize(Nelgt);
+    infile.seekg(GlelBegin, std::ifstream::beg);
     infile.read(reinterpret_cast<char *>(Glel.data()), Nelgt * sizeof(IntType));
   }
   catch (std::ifstream::failure& e)
   {
-    std::cerr << e.what() << ": Couldn't open or .fld file in binary mode: " << filename
+    std::cerr << e.what() << ": Couldn't open or .fld file in binary mode: " << Filename
               << std::endl;
     throw e;
   }
+
+  // Finally, set beginning of field data
+  FieldsBegin = GlelBegin + Nelgt * sizeof(IntType);
 }
 
 }
